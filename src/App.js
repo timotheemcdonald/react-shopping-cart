3f990621c06c490e064c8b745fe02d8f2f859d54
timotheemcdonald <timotheemcdonald@gmail.com>
2020-08-13 15:18:15 -0400

    submitting my files for the day

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,26 +7,39 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProductContext from '../src/contexts/ProductContext.js'
+import CartContext from '../src/contexts/CartContext'
+
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		console.log('add item test')
+		// const newItem = item;
+		setCart([...cart, item]);
+		console.log(item, 'new item to be added')
+		console.log(cart, 'item should be in cart')
 		// add the given item to the cart
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{addItem, products}}>
+				<CartContext.Provider value={cart}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
